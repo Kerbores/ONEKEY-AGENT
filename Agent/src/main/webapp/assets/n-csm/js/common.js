8a1d6e765d713673cb213a31bc0102bfc2e28c8e
kerbores <kerbores@gmail.com>
2016-10-12 20:04:17 +0800

    告警设置

--- Agent/src/main/webapp/assets/n-csm/js/common.js
+++ Agent/src/main/webapp/assets/n-csm/js/common.js
@@ -2,34 +2,44 @@
  * Created by kerbores on 2016/10/8.
  */
 var Common = {
-    init: function () {
-        console.log("common.js is runnning...");
-    },
-    getRootPath: function () {
-        return contextPath;
-    },
-    validationFail:function(msg,dom){
-    	layer.tips(msg,dom);
-    }
+	init : function() {
+		console.log("common.js is runnning...");
+	},
+	getRootPath : function() {
+		return contextPath;
+	},
+	validationFail : function(msg, dom) {
+		layer.tips(msg, dom);
+	},
+	showMessage : function(msg, title) {
+		if (layer) {
+			layer.msg(msg, {
+				icon : 1,
+				title : title || false
+			});
+		} else {
+			alert(msg);
+		}
+	}
 }
 
-Date.prototype.format = function (format) {
-    var date = {
-        "M+": this.getMonth() + 1,
-        "d+": this.getDate(),
-        "h+": this.getHours(),
-        "m+": this.getMinutes(),
-        "s+": this.getSeconds(),
-        "q+": Math.floor((this.getMonth() + 3) / 3),
-        "S+": this.getMilliseconds()
-    };
-    if (/(y+)/i.test(format)) {
-        format = format.replace(RegExp.$1, (this.getFullYear() + '').substr(4 - RegExp.$1.length));
-    }
-    for (var k in date) {
-        if (new RegExp("(" + k + ")").test(format)) {
-            format = format.replace(RegExp.$1, RegExp.$1.length == 1 ? date[k] : ("00" + date[k]).substr(("" + date[k]).length));
-        }
-    }
-    return format;
+Date.prototype.format = function(format) {
+	var date = {
+		"M+" : this.getMonth() + 1,
+		"d+" : this.getDate(),
+		"h+" : this.getHours(),
+		"m+" : this.getMinutes(),
+		"s+" : this.getSeconds(),
+		"q+" : Math.floor((this.getMonth() + 3) / 3),
+		"S+" : this.getMilliseconds()
+	};
+	if (/(y+)/i.test(format)) {
+		format = format.replace(RegExp.$1, (this.getFullYear() + '').substr(4 - RegExp.$1.length));
+	}
+	for (var k in date) {
+		if (new RegExp("(" + k + ")").test(format)) {
+			format = format.replace(RegExp.$1, RegExp.$1.length == 1 ? date[k] : ("00" + date[k]).substr(("" + date[k]).length));
+		}
+	}
+	return format;
 }
