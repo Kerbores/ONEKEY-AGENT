d73d2145dacfc0b4e4fe81238ad7a1625b65c2cb
王贵源 <kerbores@gmail.com>
2016-10-12 10:27:55 +0800

    解決找不到模板

--- Agent/src/main/resources/ioc/conf.js
+++ Agent/src/main/resources/ioc/conf.js
@@ -2,5 +2,13 @@ var ioc = {
     "conf": {
         "type": "club.zhcs.agent.MainConfig",
         "args": ["customer/"]
-    }
+    },
+    config : {
+		type : "org.nutz.ioc.impl.PropertiesProxy",
+		fields : {
+			ignoreResourceNotFound : true,
+			utf8 : false,
+			paths : 'conf'
+		}
+	}
 };
