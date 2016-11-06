72a415ff4b1938e89d17bf183d4890de8cb2b7e4
kerbores <kerbores@gmail.com>
2016-11-06 11:28:28 +0800

    Druid 密码配置

--- Agent/src/main/java/club/zhcs/agent/Agent.java
+++ Agent/src/main/java/club/zhcs/agent/Agent.java
@@ -3,6 +3,8 @@ package club.zhcs.agent;
 import org.nutz.ioc.impl.PropertiesProxy;
 import org.nutz.mvc.Mvcs;
 
+import com.alibaba.druid.filter.config.ConfigTools;
+
 public class Agent {
 
 	public final static String VERSION = "1.0.0";
@@ -13,6 +15,10 @@ public class Agent {
 		 */
 		public static final String USER_KEY = "KERBORES_USER";
 		public static final String WECHAT_USER_KEY = "KERBORES_WECHAT_USER";
+	}
+
+	public static void main(String[] args) throws Exception {
+		ConfigTools.main(args);
 	}
 
 	public static String config(String key) {
